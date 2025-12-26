a96fbd74fad0ccb117a00105e34ab38d45b1a50c
DESKTOP-7N0KFJK\TMP-214 <mvmii1210@gmail.com>
2025-12-26 18:10:24 +0800

    少添加

--- scripts/ApplicationStart.sh
+++ scripts/ApplicationStart.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-docker run -p 80:5000 --name flask-devops -d 706004250357.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-15-ecr-repo:latest
+docker run -p 80:5000 --name flask-web -d 706004250357.dkr.ecr.ap-northeast-1.amazonaws.com/cjc102-15-ecr-repo:latest
 
 sleep 20s
--- scripts/ApplicationStop.sh
+++ scripts/ApplicationStop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [ `docker ps | grep boring_shockley | wc -l`  = 1 ]
+if [ `docker ps | grep flask-web | wc -l`  = 1 ]
 then
         docker stop flask-web
         docker rm  flask-web
